2391fcee7128d0dc0d81b46a921b6cad0a172dc5
Justin Merz <jrmerz@gmail.com>
2019-03-05 13:23:08 -0800

    adding simple cli, docs

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # cdec-download
 CDEC Data Download Tool
-
 http://cdec.water.ca.gov
+
+# Install
+
+NodeJS/NPM is required, follow install install instructions here:
+https://nodejs.org/en/download/
+
+then run the following in shell:
+```bash
+npm install -g @ucd-lib/cdec-download
+```
+
+# Usage
+
+Open shell and run ```cdec-download``` followed by ISO 8601 date.
+
+```bash
+cdec-download [yyyy-mm-dd]
+```
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let stream = require('./lib/CdecStream');
 let utils = require('./lib/utils');
 
+if( process.argv.length <= 2 ) exit('Please provide a date');
+let date = process.argv[2];
+if( !date.match(/^\d{4}-\d{2}-\d{2}$/) ) {
+  exit('Please specify a date in ISO 8601 format: yyyy-mm-dd');
+}
+
 (async function(){
-
-  await stream.download('MPD', utils.newDate(2019, 2, 5));
-
+  let [year, month, day] = date.split('-').map(n => parseInt(n));
+  await stream.download('MPD', utils.newDate(year, month, day));
 })();
+
+function exit(msg) {
+  if( msg ) console.log(msg);
+  process.exit();
+}
